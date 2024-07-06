93c527507ce2d8cdf923facc9396b33d289b3fa0
Abhishekjani2509 <abhishekjani2509@gmail.com>
2024-07-06 12:45:35 +0530

    Frontend Started

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/layout/Header.js";
+import { BrowserRouter as Router } from "react-router-dom";
+function App() {
+  return (
+    <div>
+      <Router>
+        <Header />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
